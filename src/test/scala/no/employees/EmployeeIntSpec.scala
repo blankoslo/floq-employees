932ebb6eee480e9e3869c3d5b2409aa334620be2
Magne Davidsen <magne.davidsen@gmail.com>
2015-07-17 14:16:04 +0200

    working integration tests

--- src/test/scala/no/employees/EmployeeIntSpec.scala
+++ src/test/scala/no/employees/EmployeeIntSpec.scala
@@ -2,69 +2,35 @@ package no.employees
 
 import java.time.LocalDate
 
-import dispatch.{Http, Req, host}
-import no.employees.data.{Genders, Employee}
+import argonaut.Argonaut._
+import argonaut._
+import dispatch.Defaults._
+import dispatch.{Http, Req, host, _}
+import no.employees.JsonCodecs._
+import no.employees.data.{Entity, DbTables, Employee, Genders}
 import org.json4s.DefaultFormats
 import org.junit.runner.RunWith
-import org.json4s.native.Serialization.{read, write}
-import dispatch._, Defaults._
-import org.scalatest.{BeforeAndAfterAll, Sequential, FunSuite}
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterAll, FunSuite}
+import no.employees.data.EmployeesPostgresDriver.api._
 
-import argonaut._, Argonaut._
-import JsonCodecs._
-
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration.Duration
-
-//object databaseSetupVote extends InMemDBEnvironment with DBTestData  {
-//
-//  implicit val session = Database.forDataSource(dataSource).createSession()
-//
-//  lazy val createDb = {
-//    createAllTables
-//    println("creating the database")
-//  }
-//
-//  lazy val createTestData = {
-//    artistRepo.artists += Artist(Some(1), "artist-1", "YO")
-//    artistRepo.artists += Artist(Some(2), "artist-2", "YO")
-//    artistRepo.artists += Artist(Some(3), "artist-3", "YO")
-//  }
-//
-//  def cleanVoteTable = {
-//    voteRepo.votes.ddl.drop
-//    voteRepo.votes.ddl.create
-//  }
-//
-//  lazy val closeSession = {
-//    session.close
-//    println("closing session")
-//  }
-//}
+import scalaz.{-\/, \/-, \/}
 
 @RunWith(classOf[JUnitRunner])
-class EmployeeIntSpec extends FunSuite with InMemDBEnvironment with BeforeAndAfterAll with DBTestData {
+class EmployeeIntSpec extends FunSuite with IntegrationTestRegistry with BeforeAndAfterEach {
 
   val server = unfiltered.jetty.Server.anylocal.plan(EmployeePlan).start()
   lazy val myHost = host("localhost", server.portBindings.head.port)
   implicit val formats = DefaultFormats
-  //implicit val session = dbSetup.session
 
-
-  override def beforeAll: Unit = {
-    createAllTables
+  override def beforeEach: Unit = {
+    val action = DbTables.employeeQuery.delete
+    val future = database.run(action).map(\/-(_))
+    future.onFailure { case (t: Throwable) => -\/(t.toString) }
+    Await.result(future, Duration.Inf)
   }
-
-
-
-  //lazy val dbSetup = databaseSetupVote
-  //override def map(fs: => Fragments) = Step(dbSetup.createDb) ^ Step(dbSetup.createTestData) ^ fs ^ Step(dbSetup.closeSession)
-
-//  override def before = {
-//    println("cleaning artist table")
-//    dbSetup.cleanVoteTable
-//  }
 
   test("Post to vote should return created code") {
     val json = Employee("firstname", "lastname", "40221672", Genders.Female, LocalDate.now, Some(LocalDate.now), Some(LocalDate.now),
@@ -74,32 +40,20 @@ class EmployeeIntSpec extends FunSuite with InMemDBEnvironment with BeforeAndAft
     assert(Await.result(Http(req), Duration(3, "sec")).getStatusCode == 201)
   }
 
-//  "post to vote" should {
-//    "return created" in {
-//      val json = write(Vote(None, 1, 1, "ip", "session"))
-//      val req = TestHelper.setJsonHeaders(myHost / "api" / "vote").setBody(json).POST
-//
-//      Await.result(Http(req), Duration(3, "sec")).getStatusCode must beEqualTo(201)
-//    }
+  test("Post to vote should create employee in db") {
+    val json = Employee("", "lastname", "40221672", Genders.Male, LocalDate.now, Some(LocalDate.now), Some(LocalDate.now),
+      Some(""), Some(""), Some(""), Some(""), Some(""), Some("")).asJson.toString
+    val req = TestHelper.setJsonHeaders(myHost / "api" / "employees").setBody(json).POST
 
-//    "create votes in the database" in {
-//      val testVote = Vote(None, 1, 1, "ip", "session")
-//      val json = write(testVote)
-//      val req = TestHelper.setJsonHeaders(myHost / "api" / "vote").setBody(json).POST
-//
-//      Await.result(Http(req), Duration(3, "sec")).getStatusCode must beEqualTo(201)
-//      Database.forDataSource(dataSource) withDynSession {
-//
-//        voteRepo.votes.list.length must beEqualTo(1)
-//        voteRepo.votes.list.head.artistId must beEqualTo(testVote.artistId)
-//        voteRepo.votes.list.head.points must beEqualTo(testVote.points)
-//        voteRepo.votes.list.head.ip must beEqualTo(testVote.ip)
-//        voteRepo.votes.list.head.session must beEqualTo(testVote.session)
-//      }
-//    }
+    Await.result(Http(req), Duration(3, "sec"))
+
+    employeeRepo.getEmployeesFromRepo match {
+      case \/-(success) => assert(success.length == 1)
+      case -\/(failure) => failure
+    }
+
   }
-
-
+}
 
 object TestHelper {
   def setJsonHeaders(endpoint: Req): Req = {
