ce95cfc2e40db3517889ac1e72e6061287422b9d
Magne Davidsen <magne.davidsen@gmail.com>
2015-07-29 18:36:50 +0200

    some basic handling of mandatory fields

--- frontend/app/components/createEmployee.jsx
+++ frontend/app/components/createEmployee.jsx
@@ -9,6 +9,22 @@ var TextField = require('./formItems/textField.jsx');
 var SelectField = require('./formItems/selectField.jsx');
 var DateField = require('./formItems/dateField.jsx');
 
+var Errors = React.createClass({
+    render: function() {
+        var errors = this.props.errors;
+        var errorTexts = [];
+
+        for(var error in errors){
+            errorTexts.push(error + " er et obligatorisk felt");
+        }
+
+        return (
+            <div className="errorBlock">
+                {errorTexts.map(error => <div className="error">{error}</div>)}
+            </div>
+        )
+    }
+});
 
 var CreateEmployee = React.createClass({
     mixins: [
@@ -23,6 +39,7 @@ var CreateEmployee = React.createClass({
     getStateFromFlux() {
         var genderStore = this.getFlux().store('GenderStore');
         return {
+            errors: {},
             genders: genderStore.genders,
             employee: new Record.Employee()
         };
@@ -31,25 +48,34 @@ var CreateEmployee = React.createClass({
     handleSubmit(event) {
         event.preventDefault();
 
-        this.getFlux().actions.createEmployee(this.state.employee);
-
+        if(this.requiredFieldsAreOk()){
+            this.getFlux().actions.createEmployee(this.state.employee);
+        }
     },
 
     handleChange: function (event) {
-        console.log(event.target.name);
-        console.log(event.target.value);
-
         var newEmployee = this.state.employee.set(event.target.name, event.target.value)
-
-        console.log("event: " + event.target);
-
         this.setState({employee: newEmployee})
+    },
 
-        console.log(newEmployee.toJS());
+    requiredFieldsAreOk: function () {
+        function isEmpty(obj) {
+            return Object.keys(obj).length === 0;
+        }
 
-        //var newState = {};
-        //newState[event.target.name] = event.target.value;
-        //this.setState(newState);
+        var requiredFields = ['firstName', 'lastName', 'phone', 'gender', 'birthDate'];
+        var that = this;
+        var errors = {};
+        requiredFields.forEach( function (requiredField) {
+            var value = that.state.employee[requiredField];
+            if (!value || (String(value).trim() === '')) {
+                errors[requiredField] = 'This field is required';
+            }
+        });
+
+        this.setState({errors: errors});
+
+        return isEmpty(errors);
     },
 
     handleChangeDate: function (date, id) {
@@ -66,30 +92,34 @@ var CreateEmployee = React.createClass({
 
         return (
             <div className="formContainer">
+                <Errors errors={this.state.errors} />
+
                 <form onSubmit={this.handleSubmit}>
-                    <TextField id="firstName" label="Fornavn" value={this.state.employee.firstName} handleChange={this.handleChange} />
-                    <TextField id="lastName" label="Etternavn" value={this.state.employee.lastName} handleChange={this.handleChange} />
-                    <TextField id="phone" label="Telefon" value={this.state.employee.phone} handleChange={this.handleChange} />
-                    <SelectField id="gender" label="Kjønn" value={this.state.employee.gender} options={options} handleChange={this.handleChange}/>
-                    <DateField id="birthDate" label="Fødselsdato" value={this.state.employee.birthDate} handleChange={this.handleChangeDate} />
-                    <DateField id="dateOfEmployment" label="Ansettelsesdato" value={this.state.employee.dateOfEmployment} handleChange={this.handleChangeDate} />
-                    <TextField id="emergencyContactName" label="Nærmeste pårørende" value={this.state.employee.emergencyContactName} handleChange={this.handleChange} />
-                    <TextField id="emergencyContactPhone" label="Pårørende telefon" value={this.state.employee.emergencyContactPhone} handleChange={this.handleChange} />
-                    <TextField id="emergencyContactRelation" label="Pårørende relasjon" value={this.state.employee.emergencyContactRelation} handleChange={this.handleChange} />
-                    <TextField id="address" label="Adresse" value={this.state.employee.address} handleChange={this.handleChange} />
-                    <TextField id="postalCode" label="Postnummer" value={this.state.employee.postalCode} handleChange={this.handleChange} />
-                    <TextField id="city" label="By" value={this.state.employee.city} handleChange={this.handleChange} />
+                    <div className="form-row">
+                        <TextField id="firstName" label="Fornavn" value={this.state.employee.firstName} handleChange={this.handleChange} error={this.state.errors['firstName']} />
+                        <TextField id="lastName" label="Etternavn" value={this.state.employee.lastName} handleChange={this.handleChange} error={this.state.errors['lastName']} />
+                        <TextField id="phone" label="Telefon" value={this.state.employee.phone} handleChange={this.handleChange} error={this.state.errors['phone']}/>
+                    </div>
+                    <div className="form-row">
+                        <SelectField id="gender" label="Kjønn" value={this.state.employee.gender} options={options} handleChange={this.handleChange} error={this.state.errors['gender']}/>
+                        <DateField id="birthDate" label="Fødselsdato" value={this.state.employee.birthDate} handleChange={this.handleChangeDate} error={this.state.errors['birthDate']}/>
+                        <DateField id="dateOfEmployment" label="Ansettelsesdato" value={this.state.employee.dateOfEmployment} handleChange={this.handleChangeDate} />
+                    </div>
+                    <div className="form-row">
+                        <TextField id="emergencyContactName" label="Nærmeste pårørende" value={this.state.employee.emergencyContactName} handleChange={this.handleChange} />
+                        <TextField id="emergencyContactPhone" label="Pårørende telefon" value={this.state.employee.emergencyContactPhone} handleChange={this.handleChange} />
+                        <TextField id="emergencyContactRelation" label="Pårørende relasjon" value={this.state.employee.emergencyContactRelation} handleChange={this.handleChange} />
+                    </div>
+                    <div className="form-row">
+                        <TextField id="address" label="Adresse" value={this.state.employee.address} handleChange={this.handleChange} />
+                        <TextField id="postalCode" label="Postnummer" value={this.state.employee.postalCode} handleChange={this.handleChange} />
+                        <TextField id="city" label="By" value={this.state.employee.city} handleChange={this.handleChange} />
+                    </div>
                     <button type="submit" value="Submit">Lagre</button>
                 </form>
             </div>
         );
     }
 });
-
-
-//birthDate: null,
-//    dateOfEmployment: null, terminationDate: null, emergencyContactName: null, emergencyContactPhone: null,
-//    address: null, postalCode: null, city: null
-
 
 module.exports = CreateEmployee;
