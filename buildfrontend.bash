26f61749dd46ed3cf2e63ac3e7ba3a21bbfcc2fd
Magne Davidsen <magne.davidsen@gmail.com>
2015-09-24 15:15:41 +0200

    expecting nom to be installed

--- buildfrontend.bash
+++ buildfrontend.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-command -v npm >/dev/null 2>&1 || {sudo apt-get update;sudo install nodejs;sudo apt-get install npm }
+command -v npm >/dev/null 2>&1 || { echo >&2 "Couldn't find node/npm on path!. Try installing it (e.g. 'brew install node')"; exit 1; }
 
 [[ "$PWD" == *frontend/app ]] || cd ./frontend/app
 [[ "$PWD" == *frontend/app ]] || { echo >&2 "Couldn't locate the js folder! This script must be run from either the folder or the project root folder"; exit 1; }
