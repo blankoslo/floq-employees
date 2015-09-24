564e32d7c75fedce83eb11897b9a12b38240fb75
Magne Davidsen <magne.davidsen@gmail.com>
2015-09-24 16:25:33 +0200

    nom build doesn't work

--- buildfrontend.bash
+++ buildfrontend.bash
@@ -6,6 +6,4 @@ command -v npm >/dev/null 2>&1 || { echo >&2 "Couldn't find node/npm on path!. T
 [[ "$PWD" == *frontend/app ]] || { echo >&2 "Couldn't locate the js folder! This script must be run from either the folder or the project root folder"; exit 1; }
 
 npm install
-
-NPM_COMMAND=${1-build}
-npm run ${NPM_COMMAND}
+npm run minify
