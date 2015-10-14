2ce8b49b3b5c580ec9ad2f7c50e9a0ff3488a371
Martin Gammelsæter <martin@mg.am>
2015-10-14 11:16:43 +0200

    Move attribute labels to constants.
    Now attribute labels are only defined in one place.

--- frontend/app/components/employeeForm.jsx
+++ frontend/app/components/employeeForm.jsx
@@ -6,6 +6,9 @@ var TextField = require('./formItems/textField.jsx');
 var SelectField = require('./formItems/selectField.jsx');
 var DateField = require('./formItems/dateField.jsx');
 
+var Constants = require('../constants.js');
+let labels = Constants.ATTR_LABELS;
+
 var EmployeeForm = React.createClass({
     mixins: [
         Fluxxor.FluxMixin(React),
@@ -71,25 +74,25 @@ var EmployeeForm = React.createClass({
         return (
             <form onSubmit={this.handleSubmit}>
                 <div className="form-row">
-                    <TextField id="firstName" label="Fornavn" value={this.state.employee.firstName} handleChange={this.handleChange} error={this.state.errors['firstName']} />
-                    <TextField id="lastName" label="Etternavn" value={this.state.employee.lastName} handleChange={this.handleChange} error={this.state.errors['lastName']} />
-                    <TextField id="phone" label="Telefon" value={this.state.employee.phone} handleChange={this.handleChange} error={this.state.errors['phone']}/>
-                    <TextField id="email" label="Epost" value={this.state.employee.email} handleChange={this.handleChange} error={this.state.errors['email']}/>
+                    <TextField id="firstName" label={labels.firstName} value={this.state.employee.firstName} handleChange={this.handleChange} error={this.state.errors['firstName']} />
+                    <TextField id="lastName" label={labels.lastName} value={this.state.employee.lastName} handleChange={this.handleChange} error={this.state.errors['lastName']} />
+                    <TextField id="phone" label={labels.phone} value={this.state.employee.phone} handleChange={this.handleChange} error={this.state.errors['phone']}/>
+                    <TextField id="email" label={labels.email} value={this.state.employee.email} handleChange={this.handleChange} error={this.state.errors['email']}/>
                 </div>
                 <div className="form-row">
-                    <SelectField id="gender" label="Kjønn" value={this.state.employee.gender} options={options} handleChange={this.handleChange} error={this.state.errors['gender']}/>
-                    <DateField id="birthDate" label="Fødselsdato" value={this.state.employee.birthDate} handleChange={this.handleChangeDate} error={this.state.errors['birthDate']}/>
-                    <DateField id="dateOfEmployment" label="Ansettelsesdato" value={this.state.employee.dateOfEmployment} handleChange={this.handleChangeDate} />
+                    <SelectField id="gender" label={labels.gender} value={this.state.employee.gender} options={options} handleChange={this.handleChange} error={this.state.errors['gender']}/>
+                    <DateField id="birthDate" label={labels.birthDate} value={this.state.employee.birthDate} handleChange={this.handleChangeDate} error={this.state.errors['birthDate']}/>
+                    <DateField id="dateOfEmployment" label={labels.dateOfEmployment} value={this.state.employee.dateOfEmployment} handleChange={this.handleChangeDate} />
                 </div>
                 <div className="form-row">
-                    <TextField id="emergencyContactName" label="Nærmeste pårørende" value={this.state.employee.emergencyContactName} handleChange={this.handleChange} />
-                    <TextField id="emergencyContactPhone" label="Pårørende telefon" value={this.state.employee.emergencyContactPhone} handleChange={this.handleChange} />
-                    <TextField id="emergencyContactRelation" label="Pårørende relasjon" value={this.state.employee.emergencyContactRelation} handleChange={this.handleChange} />
+                    <TextField id="emergencyContactName" label={labels.emergencyContactName} value={this.state.employee.emergencyContactName} handleChange={this.handleChange} />
+                    <TextField id="emergencyContactPhone" label={labels.emergencyContactPhone} value={this.state.employee.emergencyContactPhone} handleChange={this.handleChange} />
+                    <TextField id="emergencyContactRelation" label={labels.emergencyContactRelation} value={this.state.employee.emergencyContactRelation} handleChange={this.handleChange} />
                 </div>
                 <div className="form-row">
-                    <TextField id="address" label="Adresse" value={this.state.employee.address} handleChange={this.handleChange} />
-                    <TextField id="postalCode" label="Postnummer" value={this.state.employee.postalCode} handleChange={this.handleChange} />
-                    <TextField id="city" label="By" value={this.state.employee.city} handleChange={this.handleChange} />
+                    <TextField id="address" label={labels.address} value={this.state.employee.address} handleChange={this.handleChange} />
+                    <TextField id="postalCode" label={labels.postalCode} value={this.state.employee.postalCode} handleChange={this.handleChange} />
+                    <TextField id="city" label={labels.city} value={this.state.employee.city} handleChange={this.handleChange} />
                 </div>
                 <button type="submit" value="Submit">Lagre</button> <button onClick={this.props.onCancel}>Avbryt</button>
             </form>
