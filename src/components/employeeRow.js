23c779c8ebf0590b140b9940fc6467d7166b98d7
Erlend Hamberg <eh@blankoslo.no>
2016-03-11 15:44:03 +0100

    Remove flow annotation

--- src/components/employeeRow.js
+++ src/components/employeeRow.js
@@ -7,29 +7,9 @@ import md5 from 'md5';
 
 import { selectEmployee } from '../actions/index';
 
-export type Employee = {
-    id : number,
-    first_name : string,
-    last_name : string,
-    title : string,
-    phone : string,
-    email : string,
-    gender : string,
-    birth_date : string,
-    date_of_employment : string,
-    termination_date : string,
-    emergency_contact_name : string,
-    emergency_contact_phone : string,
-    emergency_contact_relation : string,
-    address : string,
-    postal_code : string,
-    city : string
-};
-
 const setCurrentEmployee = (id) => {
   browserHistory.push(`/employees/${id}`);
 };
-
 const EmployeeRow = (props) => (
   <div
     className='mdl-list__item employee-list-row'
