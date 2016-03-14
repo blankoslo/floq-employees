88d93e01981d0a6774bfd9a18468188eb37298ce
Erlend Hamberg <eh@blankoslo.no>
2016-03-14 12:38:54 +0100

    Create axios instance with base url

--- src/apiclient.js
+++ src/apiclient.js
@@ -1,13 +1,17 @@
 // @flow
 
-import axios from 'axios';
+import axios_ from 'axios';
 import { apiUrl } from '../config';
 
+const axios = axios_.create({
+  baseURL: apiUrl
+});
+
 export const getEmployees = () =>
-  axios.get(`${apiUrl}/employees?order=first_name.asc`);
+  axios.get(`/employees?order=first_name.asc`);
 
 export const updateEmployee = (id, updates) =>
-  axios.patch(`${apiUrl}/employees?id=eq.${id}`, updates);
+  axios.patch(`/employees?id=eq.${id}`, updates);
 
 export const createEmployee = (data) =>
-  axios.post(`${apiUrl}/employees`, data);
+  axios.post(`/employees`, data);
