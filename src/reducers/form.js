4ef8933e911bd554fc29919d1b40087f3ae5f3cc
Erlend Hamberg <eh@blankoslo.no>
2016-05-19 17:05:23 +0200

    Fix form reducer

--- src/reducers/form.js
+++ src/reducers/form.js
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 
 import { FORM_RESET, FORM_UPDATE_VALUE } from '../actions';
 
-export const FormReducer = (state = null, action) => {
+export default (state = null, action) => {
   switch (action.type) {
     case FORM_UPDATE_VALUE:
       return _.merge({}, state, action.payload);
