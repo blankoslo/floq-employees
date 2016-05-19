624e7bf2478d47e11a9eaa75c7cadf9aa0b913b3
Erlend Hamberg <eh@blankoslo.no>
2016-05-19 13:14:08 +0200

    Simplify select employee reducer

--- src/reducers/selectEmployee.js
+++ src/reducers/selectEmployee.js
@@ -2,9 +2,7 @@ import { SELECT_EMPLOYEE } from '../actions/index';
 
 export default (state = null, action) => {
   if (action.type === SELECT_EMPLOYEE) {
-    if (action.payload === null) return null;
-
-    return Object.assign({}, action.payload);
+    return action.payload;
   }
 
   return state;
