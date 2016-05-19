3200e9ecddf8d5c8815b83f57ec0b796c6bac810
Erlend Hamberg <eh@blankoslo.no>
2016-05-19 13:15:53 +0200

    Fix indentation of jsx block

--- src/components/viewEmployee.js
+++ src/components/viewEmployee.js
@@ -31,57 +31,57 @@ const viewEmployee = ({ employee }) => {
   }
 
   return (
-  <div className='mdl-grid'>
-    <div className={`${cellClasses} center-text`}>
-    </div>
-    <div className={`${cellClasses} center-text`}>
-      <img
-        className='profile-pic'
-        src={`https://www.gravatar.com/avatar/${md5(employee.email)}`}
-        alt='{employee.first_name}'
-      />
-    </div>
-    <div className={`${cellClasses} center-text`}>
-      <h5>{employee.first_name} {employee.last_name}</h5>
-      <span className='gray'>{employee.title}</span>
-    </div>
-    <div className='vert-spacer' />
-    <div className={gridClasses}>
-      <IconAndText icon='phone' textLines={[employee.phone]} />
-    </div>
-    <div className={gridClasses}>
-      <IconAndText icon='email' textLines={[employee.email]} />
-    </div>
-    <div className={gridClasses}>
-      <IconAndText
-        icon='location_on'
-        textLines={[employee.address, `${employee.postal_code} ${employee.city}`]}
-      />
-    </div>
-    <div className='mdl-cell mdl-cell--9-col mdl-cell--3-offset-desktop mdl-cell--12-col-phone'>
-      <hr className='icon-margin' />
-    </div>
-    <div className={gridClasses}>
-      <div className='icon-row'>
-        <div className='icon-row-icon'>
-        </div>
-        <div>
-          <h3>Kontaktperson</h3>
-          <span className='emergency-contact'>{employee.emergency_contact_name}</span>
-          <br />
-          <span className='emergency-contact gray'>{employee.emergency_contact_relation}</span>
-          <br />
-          <span className='emergency-contact'>{employee.emergency_contact_phone}</span>
+    <div className='mdl-grid'>
+      <div className={`${cellClasses} center-text`}>
+      </div>
+      <div className={`${cellClasses} center-text`}>
+        <img
+          className='profile-pic'
+          src={`https://www.gravatar.com/avatar/${md5(employee.email)}`}
+          alt='{employee.first_name}'
+        />
+      </div>
+      <div className={`${cellClasses} center-text`}>
+        <h5>{employee.first_name} {employee.last_name}</h5>
+        <span className='gray'>{employee.title}</span>
+      </div>
+      <div className='vert-spacer' />
+      <div className={gridClasses}>
+        <IconAndText icon='phone' textLines={[employee.phone]} />
+      </div>
+      <div className={gridClasses}>
+        <IconAndText icon='email' textLines={[employee.email]} />
+      </div>
+      <div className={gridClasses}>
+        <IconAndText
+          icon='location_on'
+          textLines={[employee.address, `${employee.postal_code} ${employee.city}`]}
+        />
+      </div>
+      <div className='mdl-cell mdl-cell--9-col mdl-cell--3-offset-desktop mdl-cell--12-col-phone'>
+        <hr className='icon-margin' />
+      </div>
+      <div className={gridClasses}>
+        <div className='icon-row'>
+          <div className='icon-row-icon'>
+          </div>
+          <div>
+            <h3>Kontaktperson</h3>
+            <span className='emergency-contact'>{employee.emergency_contact_name}</span>
+            <br />
+            <span className='emergency-contact gray'>{employee.emergency_contact_relation}</span>
+            <br />
+            <span className='emergency-contact'>{employee.emergency_contact_phone}</span>
+          </div>
         </div>
       </div>
+      <button
+        onClick={() => browserHistory.push(`/employees/${employee.id}/edit`)}
+        className='mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab'
+      >
+        <i className='material-icons dark-gray'>create</i>
+      </button>
     </div>
-    <button
-      onClick={() => browserHistory.push(`/employees/${employee.id}/edit`)}
-      className='mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab'
-    >
-      <i className='material-icons dark-gray'>create</i>
-    </button>
-  </div>
   );
 };
 
