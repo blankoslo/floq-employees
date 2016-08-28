c40f213494fc2922fe94dc9f1e3dd9cc1f89b1e8
Erlend Hamberg <eh@blankoslo.no>
2016-08-28 13:38:01 +0200

    Update test helper

--- test/testhelper.js
+++ test/testhelper.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import reducers from '../src/reducers';
 import chaiJquery from 'chai-jquery';
 
@@ -13,7 +13,7 @@ const $ = jquery(global.window);
 // build 'renderComponent' helper that should render a given react class
 const renderComponent = (ComponentClass, props, state) => {
   const componentInstance = TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
+    <Provider store={createStore(combineReducers(reducers), state)}>
       <ComponentClass {...props} />
     </Provider>
   );
