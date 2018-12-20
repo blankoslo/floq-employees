094db2fb754fe843ffb0169dd2fd4ca64c967f70
Ole Christer Selvig <ole.selvig@blank.no>
2018-12-20 13:52:25 +0100

    Fix wrong destination path for bundle in production

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,30 +1,30 @@
 module.exports = {
   module: {
     rules: [
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-      { test: /\.json$/, loader: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+        {
+            test: /\.svg/,
+            use: {
+                loader: 'svg-url-loader',
+                options: {}
+            }
+        },
+        { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+        { test: /\.json$/, loader: 'json-loader' },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader'
+            }
+        },
+        {
+            test: [/\.(ttf|woff)$/],
+            loader: 'file-loader',
+            options: {
+            // See floq project for path
+            name: 'static/fonts/[name].[ext]'
+            }
         }
-      },
-      {
-        test: [/\.(ttf|woff)$/],
-        loader: 'file-loader',
-        options: {
-          // See floq project for path
-          name: 'static/fonts/[name].[ext]'
-        }
-      },
-      {
-        test: /\.svg/,
-        use: {
-          loader: 'svg-url-loader',
-          options: {}
-        }
-      }
     ]
   }
 };
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,13 +1,14 @@
 const webpack = require("webpack");
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   entry: ["./src/index.js"],
   output: {
-    path: __dirname + "/dist/js",
+    path: path.resolve(__dirname, "../dist/js"),
     filename: "app.bundle.js"
   },
   plugins: [
